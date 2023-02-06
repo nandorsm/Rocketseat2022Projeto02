7fc97407d70d58ae79dd2f50b8af05a250fef09d
nandorsm <fernandocaetano933@gmail.com>
2023-02-05 21:44:32 -0300

    React Hook Form

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,51 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td><Status statusColor="green">Concluído</Status></td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td><Status statusColor="yellow">Em andamento</Status></td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td><Status statusColor="red">Interrompido</Status></td>
+            </tr>
+          </tbody>
+          
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+    
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
 import {
   CountdownContainer,
   FormContainer,
@@ -9,16 +11,50 @@ import {
   TaskInput,
 } from './styles'
 
+// controlled / uncontrolled
+
 export function Home() {
+  //const [task, setTask] = useState('')
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitedDesabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em </label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+
+            {...register('task')}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" />
+          <MinutesAmountInput 
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', {valueAsNumber: true})}
+          />
+
           <span>minutos.</span>
         </FormContainer>
 
@@ -30,7 +66,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitedDesabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
