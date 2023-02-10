4e9c6e6416323f6be4045ccbb65d6901469ce850
nandorsm <fernandocaetano933@gmail.com>
2023-02-09 23:47:49 -0300

    finished forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,14 +23,26 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
   // const [task, setTask] = useState('')
-  const { register, handleSubmit, watch /*formState*/ } = useForm({
+  const { register, handleSubmit, watch, reset /*formState*/ } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   // console.log(formState.errors)
